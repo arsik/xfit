ccdb531b4d1178feae8eb67ed96190bf86228d3a
ikasimov <info@thelumio.com>
2015-11-23 16:37:16 +0300

    New changelist

---
+++ common/config/main.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language' => 'ru-RU',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
+    ],
+];
